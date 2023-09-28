16ca542684e61c1840db500d24839a37d374c13e
Igor Penaque <igor.penaque@sysmap.com.br>
2023-09-28 20:03:54 -0300

    added codrops links

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+/*
+ * Configs for codrops
+ */
+
+export const tutorialText = "Read the tutorial";
+export const tutorialHref = "https://tympanus.net/codrops/?p=73418";
+
+export const previousText = "Previous demo";
+export const previousHref = "https://tympanus.net/codrops/?p=";
