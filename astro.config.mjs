a2a1997e16f8dc7ba2a49c504f6cdd17ad18fe4b
Manoela Ilic <crnacura@gmail.com>
2023-10-03 12:38:25 +0100

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/",
+  base: "Development/AstroShop",
   integrations: [tailwind()]
 });
