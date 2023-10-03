0c6933683acf921a9184bb0cd85400eff26d123e
Manoela Ilic <crnacura@gmail.com>
2023-10-03 13:34:06 +0100

    New images

--- src/config.ts
+++ src/config.ts
@@ -9,4 +9,4 @@ export const previousText = "Previous demo";
 export const previousHref = "http://tympanus.net/Development/SlideshowAnimations/";
 
 export const githubText   = "GitHub"
-export const githubHref   = "https://github.com/igorm84/astro-shop-view-transitions"
+export const githubHref   = "https://github.com/codrops/astro-shop-view-transitions"
