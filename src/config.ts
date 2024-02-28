0b431f92e10c693e156ee7644ac0f9f179407b04
botelho <botelho@users.noreply.github.com>
2024-02-28 09:52:06 +0000

    Update config.ts
    Link to Codrops all demos page

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@
 export const tutorialText = "Read the tutorial";
 export const tutorialHref = "https://tympanus.net/codrops/?p=73418";
 
-export const previousText = "Previous demo";
-export const previousHref = "http://tympanus.net/Development/SlideshowAnimations/";
+export const previousText = "All demos";
+export const previousHref = "http://tympanus.net/codrops/demos/";
 
 export const githubText   = "GitHub"
 export const githubHref   = "https://github.com/codrops/astro-shop-view-transitions"
